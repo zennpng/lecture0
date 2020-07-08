b7055b7e917e1ac9af714bbd5511b511613d5101
zennpng <48861561+zennpng@users.noreply.github.com>
2020-07-08 11:55:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+let id = 0   // for labelling the todos 
+
+const Todo = props => (
+  <li>
+    <input 
+    type="checkbox" 
+    checked={props.todo.checked}   // track checked state
+    onChange={props.onToggle}   // track toggle state 
+    />
+    <button onClick={props.onDelete}>delete</button>
+    <span>{props.todo.text}</span>
+  </li>
+)
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {   // add todos to app state
+      todos: [],
+    }
+  }
+
+  addTodo() {
+    const text = prompt("Enter your TODOs here!")
+    this.setState({   // set todos state
+      todos: [
+        ...this.state.todos,   // ... creates a new array with contents of the array after the ...
+        {id: id++,   // enable id tracker
+         text: text, 
+         checked: false},   // set initial checked state
+       ], 
+    })
+  }
+
+  removeTodo(id) {   
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)   // filter the todos that are not of input id
+    })
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo   // dont do anything for todos that are not of input id
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked,   // change state of checked when todo is toggle 
+        }
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Todo count: {this.state.todos.length}</div>   
+        <div>Unchecked todo count: {this.state.todos.filter(todo => !todo.checked).length}</div>  
+        <button onClick={() => this.addTodo()}>Add TODO</button>
+        <ul>
+          {this.state.todos.map(todo => (
+            <Todo    // the todo react component 
+              onToggle={() => this.toggleTodo(todo.id)}  // run func when checkbox is toggled 
+              onDelete={() => this.removeTodo(todo.id)}  // run func when delete button is clicked
+              todo={todo}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'));
